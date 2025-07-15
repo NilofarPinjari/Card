248ddc8b0f34c7312d0531e8f2b40fc80cc61971
NilofarPinjari <nilofarpinjari2002@gmail.com>
2025-07-15 14:23:05 +0530

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 
-<<<<<<< HEAD
-=======
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
->>>>>>> 219f24d5609b070540da2460e1c3e8238bf41dcf
 import './App.css'
 
 import Input from "./Input.jsx"
